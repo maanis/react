d5259e602a86efa843f19cc6d2c436d23b84a7cf
Manish Jha <manishjha962@gmail.com>
2024-07-20 21:35:49 -0700

    try

--- 02tailwindProps/src/App.jsx
+++ 02tailwindProps/src/App.jsx
@@ -5,14 +5,14 @@ import './App.css'
 import Card from './assets/components/card'
 
 function App() {
-  let arr = [1,2,3]
+  let arr = [1, 2, 3];
   return (
     <>
       <h1 className='bg-green-400 p-4 rounded-md cursor-pointer hover:bg-green-200 hover:text-black transition-all'>Tailwind CSS</h1>
 
-      <Card name = "manish" myArr = {arr} view = "get profile"/>
-      <Card name = "raunak"/>
-      
+      <Card name="manish" myArr={arr} view="get profile" />
+      <Card name="raunak" />
+
 
     </>
   )
